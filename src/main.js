975e23a9e7bcd6c343061dd1a6356bd12d6ec414
itsApurba <apurba.apurbaadhikary@gmail.com>
2024-01-10 19:18:41 +0530

    Apify

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@
 import { PlaywrightCrawler, ProxyConfiguration } from "crawlee";
 import { router } from "./routes.js";
 import fs from "fs";
+import { Actor } from "apify";
 
-let isAuth = fs.existsSync("./auth");
+// let isAuth = fs.existsSync("./auth");
+// let session = [];
+// if (isAuth) {
+//   session = JSON.parse(fs.readFileSync("./auth"));
+// }
 
-let session = [];
-
-if (isAuth) {
-  session = JSON.parse(fs.readFileSync("./auth"));
-}
 
 const crawler = new PlaywrightCrawler({
   // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
@@ -22,21 +22,24 @@ const crawler = new PlaywrightCrawler({
   ],
   requestHandlerTimeoutSecs: 60000,
   requestHandler: router,
-  headless: false,
+  // headless: false,
   // Comment this option to scrape the full website.
   // maxRequestsPerCrawl: 20,
 });
 
 // await crawler.addRequests([
- 
+
 // ],{
 //   waitBetweenBatchesMillis: 3000
 // });
 // await crawler.addRequests([{ url: "https://www.linkedin.com/in/swapnilhota/", label: "people", userData: { designation: "backend developer" } }]);
 
-await crawler.run([
-  { url: "https://www.linkedin.com/company/nobroker-in/", userData: { designation: "frontend developer" }, useExtendedUniqueKey: true },
-  { url: "https://www.linkedin.com/company/nobroker-in/", userData: { designation: "backend developer" }, useExtendedUniqueKey: true },
-],{
-  waitBetweenBatchesMillis: 3000,
-});
+await crawler.run(
+  [
+    { url: "https://www.linkedin.com/company/nobroker-in/", userData: { designation: "frontend developer" }, useExtendedUniqueKey: true },
+    { url: "https://www.linkedin.com/company/nobroker-in/", userData: { designation: "backend developer" }, useExtendedUniqueKey: true },
+  ],
+  {
+    waitBetweenBatchesMillis: 3000,
+  }
+);
